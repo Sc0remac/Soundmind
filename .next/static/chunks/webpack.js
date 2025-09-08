b82c8427ae4cbfe5105c2802f344b8f9ef27b218
Cormac Kerr <kerrcormac@gmail.com>
2025-09-08 01:51:01 +0100

    removed .next shite

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c80af358a9cbc78e"; }
+/******/ 		__webpack_require__.h = function() { return "33acf37d8b190668"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
