d52ac4296f2bdea6f47fab05f6be22187ece820d
Cormac Kerr <kerrcormac@gmail.com>
2025-09-12 20:02:17 +0100

    Updated node packages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "i.scdn.co" },               // Spotify album art
+      { protocol: "https", hostname: "lastfm.freetls.fastly.net" },// Last.fm images (if used)
+      { protocol: "https", hostname: "i.ytimg.com" },             // YouTube thumbs (optional)
+    ],
+  },
+};
 
 module.exports = nextConfig;
